33582447fdc98d1a8af13ad4005802eac41ab29f
Lonny Snyder <Lonnysnyder81@gmail.com>
2017-03-28 22:28:14 -0500

    completed version with some bad css style

--- main.py
+++ main.py
@@ -22,6 +22,7 @@ verify=""
 username=""
 uner=""
 pwer=""
+pmater=""
 email=""
 emer=""
 
@@ -29,9 +30,15 @@ def isusernamevalid(username):
     USER_RE = re.compile(r"^[a-zA-Z0-9_-]{3,20}$")
     return USER_RE.match(username)
 
-def ispwordvalid(password,verify):
+def ispwordvalid(password):
     USER_RE = re.compile(r"^.{3,20}$")
-    if password==verify and USER_RE.match(password):
+    if USER_RE.match(password):
+        return True
+    else:
+        return False
+
+def ispwordmatch(password,verify):
+    if password==verify:
         return True
     else:
         return False
@@ -44,19 +51,33 @@ def isemailvalid(email):
         return False
 
 
-def build_page(username,uner,pwer,email,emer):
+def build_page(username,uner,pwer,pmater,email,emer):
+    head="""<!DOCTYPE html>
+                <html>
+                    <head>
+                        <title>
+                            User Signup
+                        </title>
+                        <style>
+                        body {background-color: powderblue;}
+                        h1   {color: blue;}
+                        .error    {color: red;}
+                        </style>
+                    </head>
+                    <body>"""
     headline="<h1>User Signup</h1>"
     table="""<table>
                 <tbody>
-            <tr><td><label>Username</label></td><td><input name=username type=text value=%s>%s</td></tr>
-            <tr><td><label>Password</label></td><td><input name=password type=password></td></tr>
-            <tr><td><label>Confirm Password</label></td><td><input name=verify type=password>%s</td></tr>
-            <tr><td><label>E-mail(optional)</label></td><td><input name=email type=text value=%s>%s</td></tr>
+            <tr><td><label>Username</label></td><td><input name=username type=text value=%s></td>%s</tr>
+            <tr><td><label>Password</label></td><td><input name=password type=password></td>%s</tr>
+            <tr><td><label>Confirm Password</label></td><td><input name=verify type=password></td>%s</tr>
+            <tr><td><label>E-mail(optional)</label></td><td><input name=email type=text value=%s></td>%s</tr>
                 </tbody>
             </table>
-            """%(username,uner,pwer,email,emer)
+            </body>
+            """%(username,uner,pwer,pmater,email,emer)
     submit="<input type= 'submit'/>"
-    form="<form method=post>"+headline+table+submit+"</form>"
+    form="<form method=post>"+head+headline+table+submit+"</form>"
     return form
 
 def build_success_page(username):
@@ -65,7 +86,7 @@ def build_success_page(username):
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
-        content=build_page("","","","","")
+        content=build_page("","","","","","")
         self.response.write(content)
 
     def post(self):
@@ -73,22 +94,26 @@ class MainHandler(webapp2.RequestHandler):
         password=self.request.get("password")
         verify=self.request.get("verify")
         email=cgi.escape(self.request.get("email"))
-        if isusernamevalid(username) and ispwordvalid(password,verify) and isemailvalid(email):
+        if isusernamevalid(username) and ispwordvalid(password) and ispwordmatch(password,verify) and isemailvalid(email):
             self.redirect("/Welcome?username="+username)
         else:
             if isusernamevalid(username):
                 uner=""
             else:
-                uner="Your username is invalid."
-            if ispwordvalid(password,verify):
+                uner="<td class='error'>Your username is invalid.</td>"
+            if ispwordvalid(password):
                 pwer=""
             else:
-                pwer="Your passwords don't match or are not 3-20 chars."
+                pwer="<td class='error'>Your password is not 3-20 chars."
+            if ispwordmatch(password,verify):
+                pmater=""
+            else:
+                pmater="<td class='error'>Your passwords don't match</td>"
             if isemailvalid(email):
                 emer=""
             else:
-                emer="Your email address appears to be invalid."
-            content=build_page(username,uner,pwer,email,emer)
+                emer="<td class='error'>Your email address appears to be invalid.</td>"
+            content=build_page(username,uner,pwer,pmater,email,emer)
             self.response.write(content)
 
 class SuccessPage(webapp2.RequestHandler):
